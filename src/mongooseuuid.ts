e217ab706cb4fc45cb696ffd45891d4c9069968b
Abdelhak <abdelhak@akermi.me>
2019-10-14 20:33:24 +0300

    fix mismatched field name

--- src/mongooseuuid.ts
+++ src/mongooseuuid.ts
@@ -7,6 +7,10 @@ export function mongooseuuid(schema, options) {
   schema.pre('save', async function (next) {
     const doc = this;
     const opts= options || Object.assign({},_defaultoptions)
+    opts.field=opts.field|| _defaultoptions.field
+    if(!schema.obj[opts.field]){
+      throw new Error(`you must have ${opts.field} in your schema`)
+    }
     let isfounded = true;
     let randomuid = uuid.v4();
     while (isfounded) {
@@ -15,6 +19,7 @@ export function mongooseuuid(schema, options) {
         randomuid = uuid.v4();
       } else {
         isfounded = false;
+       
         doc[opts.field] = randomuid;
         next();
       }
