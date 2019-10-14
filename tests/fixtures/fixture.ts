9d0d379bf326d3dd8a6d1d0d78aeb308548a9e0f
Abdelhak <abdelhak@akermi.me>
2019-10-14 20:33:29 +0300

    add schema

--- tests/fixtures/fixture.ts
+++ tests/fixtures/fixture.ts
@@ -4,10 +4,15 @@ import mongoose from "mongoose";
 import { Schema, Document, Model } from "mongoose";
 import { mongooseuuid } from "../../src/index";
 export type TypeDummy = IDummy & Document;
+export type TypeDummy2 = IDummy2 & Document;
 interface IDummy {
  uniqueid: string;
  name: string;
 }
+interface IDummy2 {
+    uniqueid: string;
+    name: string;
+   }
 
 
 const modelSchema = new Schema({
@@ -17,4 +22,12 @@ const modelSchema = new Schema({
 modelSchema.plugin(mongooseuuid, {
     field: "uniqueid"
 });
+const modelSchema2 = new Schema({
+    uniqueid: {type: String},
+    name: {type: String}
+});
+modelSchema2.plugin(mongooseuuid, {
+    field: "uuid"
+});
 export const dummy: Model<TypeDummy> = mongoose.model("dummy", modelSchema);
+export const dummy2: Model<TypeDummy2> = mongoose.model("dummy2", modelSchema2);
