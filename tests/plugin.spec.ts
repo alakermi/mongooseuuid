749dfce6bd04f75a359b7dbbcb0dc6fe106bc2fa
Abdelhak <abdelhak@akermi.me>
2019-10-14 20:33:07 +0300

    should throw error

--- tests/plugin.spec.ts
+++ tests/plugin.spec.ts
@@ -1,19 +1,20 @@
 
 import test from "ava";
 import request from "supertest";
-import { dummy } from "./fixtures/fixture";
+import { dummy,dummy2 } from "./fixtures/fixture";
 import { before,beforeEach, afterEach,after } from "./utils";
 
 test.beforeEach(beforeEach)
 test.afterEach(afterEach)
 test.before(before)
 test.after(after)
-
+// test.after.always(after);
 test.serial("no error same count as data", async (t) => {
   const count = await dummy.find({}).countDocuments()
 
   t.is(count,601)
 });
+
 test.serial("no duplicate", async (t) => {
   const firstrecord = await dummy.find({})
   const prev=firstrecord[0]
@@ -22,55 +23,11 @@ test.serial("no duplicate", async (t) => {
   // console.log(firstrecord);
   t.is(count,1)
 });
-test.after.always(after);
-// import mongoose from "mongoose";
-// import * as async from "async";
 
-// const data = require("./data.json");
-// async function fillDummy() {
-//     data.forEach(async (e) => {
-//         const record = await dummy.create({name: e});
-//         await record.save();
-//     });
-//     // async.forEachOf(data, async (item) => {
+test.serial("should throw error no exist field name", async (t) => {
+  await t.throwsAsync(async () => {
+		return await dummy2.create({name:"name"})
+	});
 
-//     // });
-
-// }
-// async function rmData() {
-//     await dummy.deleteMany({});
-// }
-
-
-
-// describe("mongoose uuid plugin ", () => {
-
-//     beforeAll(async () => {
-//         mongoose.connect(global.__MONGO_URI__, {
-//           useNewUrlParser: true
-//     }, (err) => {
-//           if (err) {
-//             console.error(err);
-//             process.exit(1);
-//         }
-//         });
-//         await fillDummy();
-
-//       });
-//       afterAll(async () => {
-//         console.log("calling");
-//         await rmData();
-//         // await mongoose.disconnect((err => {
-//         //   if (err) {
-//         //     console.error(err);
-//         //     process.exit(1);
-//         // }
-//         // }));
-//       });
-
-//     it("all data saved", async () => {
-//         const x = await dummy.find({}).countDocuments();
-//         expect(x).toBe(601);
-//     });
-// });
+});
 
